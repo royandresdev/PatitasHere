b75d01ed21574d96874726efde6b42ef598d1d29
royandresdev <royhuamanavila@gmail.com>
2025-10-12 00:00:21 -0500

    feat: ajustar visibilidad del botón de navegación en dispositivos grandes y eliminar código de navegación no utilizado

--- frontend/src/components/header_a.jsx
+++ frontend/src/components/header_a.jsx
@@ -65,40 +65,11 @@ const StickyNavbar = () => {
         <img className='h-full w-auto' src={logo} alt='Logo' />
       </Link>
       <button
-        className='md:hidden text-xl text-gray-800 bg-transparent border-0 cursor-pointer'
+        className='lg:hidden text-xl text-gray-800 bg-transparent border-0 cursor-pointer'
         onClick={toggleNav}
       >
         <Icon icon="iconamoon:menu-burger-horizontal-fill" className="text-xl" />
       </button>
-      <nav
-        className={`w-full z-10 fixed top-0 right-0 bottom-0 bg-pink-default text-black p-8 shadow-md transition-all duration-300 ease-in-out ${isNavVisible ? 'opacity-100 visible' : 'opacity-0 invisible'
-          } md:static md:flex md:items-center md:gap-4 md:p-0 md:bg-transparent md:shadow-none md:opacity-100 md:visible`}
-        id='nav'
-      >
-        <button
-          className='absolute right-9 font-semibold bg-transparent border-0 cursor-pointer md:hidden'
-          onClick={toggleNav}
-        >
-          <span className='w-20 text-3xl'>X</span>
-        </button>
-        <ul className='w-full h-full items-center md:justify-end justify-center list-none flex flex-col lg:gap-8 gap-[60px] md:flex-row md:items-center md:gap-4'>
-          {currentRoutes.map((item) => (
-            <li key={item.name}>
-              {item.url === '' ? (
-                <LogoutButton />
-              ) : (
-                <Link
-                  to={item.url}
-                  onClick={toggleNav}
-                  className='flex w-full text-center text-xl md:font-medium font-semibold text-black no-underline'
-                >
-                  {item.name}
-                </Link>
-              )}
-            </li>
-          ))}
-        </ul>
-      </nav>
     </header>
   )
 }
